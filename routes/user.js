8c0145f1cc6641997c76538f052e8b7a107721e2
BecomeFaisal <141523229+BecomeFaisal@users.noreply.github.com>
2024-12-27 22:27:26 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAysnc.js");
 const passport = require("passport");
 const { ref } = require("joi");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController  = require("../controllers/users.js");
 const listingController = require("../controllers/listings.js");
+const {isLoggedIn, isOwner,validateListings} = require("../middleware.js");
+const wrapAysnc = require("../utils/wrapAysnc.js")
+
 
 
 router.route("/").get(wrapAysnc(listingController.index)).post(isLoggedIn,
